205caa000acff2e22f28ec116d0204cbaa613ec2
unknown <vinothsquare110@gmail.com>
2021-04-20 10:55:20 +0530

    Double Buffer Image

--- src/javagame/JavaGame.java
+++ src/javagame/JavaGame.java
@@ -1,6 +1,7 @@
 package javagame;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -9,6 +10,8 @@ import javax.swing.JFrame;
 public class JavaGame extends JFrame{
 	
 	int x,y;
+	private Image dbImage;
+	private Graphics dbg;
 	public class AL extends KeyAdapter{
 		
 		public void keyPressed(KeyEvent e) {
@@ -52,8 +55,15 @@ public class JavaGame extends JFrame{
 		y=150;
 	}
 
+	public void paint (Graphics g) {
+		dbImage = createImage(getWidth(), getHeight());
+		dbg= dbImage.getGraphics();
+		paintComponent(dbg);
+		g.drawImage(dbImage, 0, 0, this);
+		
+	}
 	
-	  public void paint(Graphics g)
+	  public void paintComponent(Graphics g)
 	  {
 		  g.fillOval(x, y, 15, 15);
 		  repaint();
